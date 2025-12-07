ca5cb5f6ce56b95fe28c7e88282e0cd0e19a02c7
Ain Farhana <nufahana05@gmail.com>
2025-12-07 13:59:56 +0800

    Update navbar design and add cart count

--- src/Component/Assets/Navbar/Navbar.jsx
+++ src/Component/Assets/Navbar/Navbar.jsx
@@ -1,16 +1,32 @@
 import React from 'react' //6.9k (gzipped: 2.7k)
 import './Navbar.css'
-import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart-icon.png'
+import { BsShop } from "react-icons/bs";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+//import logo from '../Assets/logo.png'
+//import cart_icon from '../Assets/cart-icon.png'
 
 const Navbar = () => {
     return (
-        <div>
-            <div className="nav-logo">
-                <img src={logo} alt="" />
-                <p>SHOPPER</p>
-            </div>
+    <div className="navbar">
+        <div className="nav-logo">
+            <BsShop size={50}/>
+            <p>SHOPPER</p>
         </div>
-    )
+
+        <ul className="nav-menu">
+            <li>Shop <hr/></li>
+            <li>Men</li>
+            <li>Woman</li>
+            <li>Kids</li>
+        </ul>
+
+        <div className="nav-login-cart">
+            <button>Login</button>
+            <HiOutlineShoppingBag size={30}/>
+            <div className="nav-cart-count">0</div>
+        </div>
+    </div>
+    );
 }
+
 export default Navbar
