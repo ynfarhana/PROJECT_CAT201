4f97c0b2013e7892aa2bb94ff540234b931b513f
Wan Iman Delaila <imandelaila2005@gmail.com>
2026-01-11 00:10:42 +0800

    modify ShopCategory

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,26 +1,46 @@
-import { products } from "../Component/Assets/all_product";
-import Item from "../Component/Item/Item";
-import Footer from "../Component/Footer/Footer";
+import React, { useContext } from 'react';
+import './ShopCategory.css'; 
+import { ShopContext } from '../Context/ShopContext'; 
+import Item from '../Component/Item/Item';
+import Footer from '../Component/Footer/Footer';
 
-const ShopCategory = ({ category }) => {
-  return (
-    <div className="shop-category">
-      <div className="shopcategory-products">
-        {products
-          .filter(item => item.category === category)
-          .map(item => (
-            <Item
-              key={item.id}
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              price={item.price}
-            />
-          ))}
-      </div>
-      <Footer />
-    </div>
-  );
-};
+const ShopCategory = (props) => {
+    // Get the live data from the Warehouse (Context)
+    const { all_product } = useContext(ShopContext);
+
+    return (
+        <div className="shop-category">
+            
+            {/* Sort/Banner Section */}
+            <div className="shopcategory-indexSort">
+                <p><span>Showing Products</span></p>
+            </div>
+
+            <div className="shopcategory-products">
+                {all_product.map((item, i) => {
+                    // Safety Check: Convert "Men" to "men" so they match
+                    const pageCategory = props.category ? props.category.toLowerCase() : "";
+                    const itemCategory = item.category ? item.category.toLowerCase() : "";
+
+                    if (pageCategory === itemCategory) {
+                        return (
+                            <Item 
+                                key={i} 
+                                id={item.id} 
+                                name={item.name} 
+                                image={item.image} // Passes the image URL string
+                                price={item.price} 
+                            />
+                        );
+                    } else {
+                        return null;
+                    }
+                })}
+            </div>
+            
+            <Footer/>
+        </div>
+    );
+}
 
 export default ShopCategory;
